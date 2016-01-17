36ebb4f25ded6e975efc2aa1713f90e4603bc941
johnhellqvist <johnhellqvist@John-Mikaels-MacBook-Pro-812.local>
2016-01-18 01:01:24 +1100

    add first changes to tournament.sql, create database and tables

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,13 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- DROP TABLE IF EXISTS dbo.Product
+
+DROP DATABASE IF EXISTS tournament;
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS matches;
+CREATE DATABASE tournament;
+\c tournament;
+CREATE TABLE players(name text, id serial primary key);
+CREATE TABLE matches(id serial primary key, winner int references players(id), loser int references players(id));
 
